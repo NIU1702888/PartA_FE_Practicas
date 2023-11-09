23e00e894e0525d21aa3d14218803f00180824c8
andrei <azarin.andrei@gmail.com>
2023-11-09 10:48:40 +0100

    Opció-Menu

--- HBBQO.sh
+++ HBBQO.sh
@@ -1,1 +1,30 @@
 #!/bin/bash
+$1
+
+echo 1. Llistats de les pel·lícules del catàleg.
+echo 2. Cerca d\'una pel·lícula del catàleg.
+echo 3. Joc de preguntes sobre les pel·lícules.
+echo 4. Gestió de la base de dades de les pel·lícules
+echo 0. Sortir.
+echo Introduce la opción:
+read op
+while [[ $op < 0 ] && [ $op > 5 ]]
+do
+clear
+sleep 2 &
+	echo 1. Llistats de les pel·lícules del catàleg.
+	echo 2. Cerca d\'una pel·lícula del catàleg.
+	echo 3. Joc de preguntes sobre les pel·lícules.
+	echo 4. Gestió de la base de dades de les pel·lícules
+	echo 0. Sortir.
+	echo Introduce la opción:
+	read op
+done
+if [[ $op == 0 ]]
+then
+	exit 1
+elif [[ $op > 0 ] && [ $op < 5 ]]
+else
+	clear
+	echo En desenvolupament
+fi
