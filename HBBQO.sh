370ca432bd3a3e58cee57cbdd159d94aabe02fa9
andrei <azarin.andrei@gmail.com>
2023-11-09 12:03:17 +0100

    Millorar funcions

--- HBBQO.sh
+++ HBBQO.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 $1
-mostrar_menu(){
+mostrarMenu(){
+echo ------------------------------------------
+echo Base de dades del catàleg HBBQO
+echo ------------------------------------------
 echo 1. Llistats de les pel·lícules del catàleg.
 echo 2. Cerca d\'una pel·lícula del catàleg.
 echo 3. Joc de preguntes sobre les pel·lícules.
@@ -8,18 +11,23 @@ echo 4. Gestió de la base de dades de les pel·lícules
 echo 0. Sortir.
 echo Introduce la opción:
 }
-mostrar_opcio(){
-clear && echo En desenvolupament
+enDesenvolupament(){
+clear && echo En desenvolupament && read something && clear
 }
-mostrar_novalida(){
-clear && sleep 2 && echo Opcio no valida
+opcioNoValida(){
+echo Opcio no valida && sleep 2 && clear
 }
-mostrar_menu
+mostrarMenu
 read op
 while [[ $op != 1 &&  $op != 2  &&  $op != 3  &&  $op != 4  && $op != 0 ]]
 do
-mostrar_novalida
-mostrar_menu
+opcioNoValida
+mostrarMenu
 read op
 done
-mostrar_opcio
+if [[ $op = 0 ]]
+then
+echo Agraim la seva visita a HBBQO
+else
+enDesenvolupament
+fi
