9437b360a0b539fb771338e30311629bc52decda
andrei <azarin.andrei@gmail.com>
2023-11-09 11:37:24 +0100

    Avançar funcions menu

--- HBBQO.sh
+++ HBBQO.sh
@@ -8,25 +8,18 @@ echo 4. Gestió de la base de dades de les pel·lícules
 echo 0. Sortir.
 echo Introduce la opción:
 }
-read op
-while [[ $op < 0 ] && [ $op > 5 ]]
-do
-clear
-sleep 2 &
+mostrar_opcio(){
+clear && echo En desenvolupament
+}
+mostrar_novalida(){
+clear && sleep 2 && echo Opcio no valida
+}
 mostrar_menu
-	echo 1. Llistats de les pel·lícules del catàleg.
-	echo 2. Cerca d\'una pel·lícula del catàleg.
-	echo 3. Joc de preguntes sobre les pel·lícules.
-	echo 4. Gestió de la base de dades de les pel·lícules
-	echo 0. Sortir.
-	echo Introduce la opción:
-	read op
+read op
+while [[ $op != 1 &&  $op != 2  &&  $op != 3  &&  $op != 4  && $op != 0 ]]
+do
+mostrar_novalida
+mostrar_menu
+read op
 done
-if [[ $op == 0 ]]
-then
-	exit 1
-elif [[ $op > 0 ] && [ $op < 5 ]]
-else
-	clear
-	echo En desenvolupament
-fi
+mostrar_opcio
