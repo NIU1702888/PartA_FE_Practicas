dd2b078d6ef073d9f4c42f9094ce038b64ec93e4
NIU1702888 <azarin.andrei@gmail.com>
2023-11-16 21:32:22 +0100

    rename files

--- Tasca1-3.sh
+++ Tasca1-3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 document=$1
 ordenarvaloracio(){
-sort -n -r -t , -k 5 $document | uniq > llista.txt
-lin=$(wc -l < llista.txt)
+sort -n -r -t , -k 5 $document | uniq > rating_llista.txt
+lin=$(wc -l < rating_llista.txt)
 for i in $(seq 1 $(($lin-1)))
 do
 if [ $i -le $lin ]
@@ -12,20 +12,20 @@ clear
 else
 echo listo
 fi
-title=$(head -$i llista.txt| tail -1| cut -d, -f1)
-rating=$(head -$i llista.txt| tail -1| cut -d, -f5)
-year=$(head -$i llista.txt| tail -1| cut -d, -f4)
-desc=$(head -$i llista.txt| tail -1| cut -d, -f3)
-clas=$(head -$i llista.txt| tail -1| cut -d, -f2)
-size=$(head -$i llista.txt| tail -1| cut -d, -f6)
-echo "*****************************************************************************************************************************" >> output.txt
-echo "*Títol:	"$title	>> output.txt
-echo "*Any: "$year"	*Nivell de classificació:	"$clas 	>> output.txt
-echo "*Descripció:	"$desc 	>> output.txt
-echo "*Valoració dels usuaris: "$rating"	*Mida de la mostra:	"$size 	>> output.txt
+title=$(head -$i rating_llista.txt| tail -1| cut -d, -f1)
+rating=$(head -$i rating_llista.txt| tail -1| cut -d, -f5)
+year=$(head -$i rating_llista.txt| tail -1| cut -d, -f4)
+desc=$(head -$i rating_llista.txt| tail -1| cut -d, -f3)
+clas=$(head -$i rating_llista.txt| tail -1| cut -d, -f2)
+size=$(head -$i rating_llista.txt| tail -1| cut -d, -f6)
+echo "*****************************************************************************************************************************" >> rating_output.txt
+echo "*Títol:	"$title	>> rating_output.txt
+echo "*Any: "$year"	*Nivell de classificació:	"$clas 	>> rating_output.txt
+echo "*Descripció:	"$desc 	>> rating_output.txt
+echo "*Valoració dels usuaris: "$rating"	*Mida de la mostra:	"$size 	>> rating_output.txt
 done
 
 }
-echo "************************   HBBQO: El Nostre catàleg de ratings segons els usuaris  es:   ************************************" > output.txt
+echo "************************   HBBQO: El Nostre catàleg de ratings segons els usuaris  es:   ************************************" > rating_output.txt
 ordenarvaloracio
-more output.txt
+more rating_output.txt
