7b4ea9649477b62f54f3de9b747fdbb454326911
NIU1702888 <azarin.andrei@gmail.com>
2023-11-17 13:15:31 +0100

    actualizar script ocio-1

--- Opcio-1.sh
+++ Opcio-1.sh
@@ -10,23 +10,11 @@ echo 1-3 Mostrar el catàleg per valoració.
 echo 0. Tornar al menu anterior. 
 echo Introduce la opción:
 }
-enDesenvolupament(){
-clear && echo En desenvolupament && read something && clear
-}
-opcioNoValida(){
-echo Opcio no valida && sleep 2 && clear
-}
+
 mostrarMenu
 read op
-while [[ $op != 1 &&  $op != 2  &&  $op != 3  &&  $op != 4  && $op != 0 ]]
-do
-opcioNoValida
-mostrarMenu
-read op
-done
-if [[ $op = 0 ]]
-then
-echo Agraim la seva visita a HBBQO
-else
-enDesenvolupament
-fi
+case $op in
+1) echo Ordenar por titulo;;
+2) echo Ordenar por año;;
+3) echo Ordenar por valoración;; 
+esac
